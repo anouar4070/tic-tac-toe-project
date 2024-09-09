5e2711f0bf3b5df4a75f1de455715bfb6adb0774
anouar garbaa <g.anouar@yahoo.com>
2024-09-09 19:46:09 +0100

    Lifting activePlayer state Up

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,16 +6,18 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
 const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
 function handleSelectSquare(rowIndex, colIndex) {
   // state that depends on objects or arrays should be updated in an immutable way
   setGameBoard((prevGameBoard)=> {
     const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-    updatedBoard[rowIndex][colIndex] = 'X';
+    updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
     return updatedBoard;
   });
+
+  onSelectSquare();
 }
  
   return (
@@ -35,7 +37,7 @@ function handleSelectSquare(rowIndex, colIndex) {
   );
 }
 
-/** why we use a second spread ([...innerArray]) ?
+/** why we use a second spread operator ([...innerArray]) ?
 
 The inner spread operator ([...innerArray]) creates a shallow copy of each inner array (each row in the game board). Without this, if you modified an element in the copied array, it would still affect the original inner arrays because arrays are reference types.
 This step ensures that even if you modify a specific square in the game board, only the new copy of that row is modified, not the original one.
