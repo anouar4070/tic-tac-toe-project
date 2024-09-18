ecd3d2ec9b25436699b79495abaf0aa7ca1dcab2
anouar garbaa <g.anouar@yahoo.com>
2024-09-18 09:16:34 +0100

    add restart button logic

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array=> [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -75,6 +75,10 @@ const hasDraw = gameTurns.length === 9 && !winner;
     });
   }
 
+function handleRestart() {
+  setGameTurns([]);
+}
+
   return (
     <main>
       <div id="game-container">
@@ -90,7 +94,7 @@ const hasDraw = gameTurns.length === 9 && !winner;
             isActive={activePlayer === "O"}
           />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} />}
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
         <GameBoard onSelectSquare={handleSelectedSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
